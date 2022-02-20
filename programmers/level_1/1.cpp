96fde29b53487780a2a4d63162aefe05e70ef8f9
GHW0826 <96938324+GHW0826@users.noreply.github.com>
2022-02-20 17:23:13 +0900

    Update 1.cpp

--- programmers/level_1/1.cpp
+++ programmers/level_1/1.cpp
@@ -1,21 +1,34 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+// 1. 정렬 비교.
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    int max_size = max(participant.size(), completion.size());
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
+    int i = 0;
+    for (; i < completion.size(); ++i)
+        if (participant[i] != completion[i])
+            return completion[i];
     
-    for (int i = 0; i < max_size; ++i) {
-        if (participant[i] != completion[i]) {
+    return participant[i];
+}
+
+// 2. 해시
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> table;    
+    for (int i = 0; i < completion.size(); ++i)
+        table[completion[i]]++;
+        
+    for (int i = 0; i < participant.size(); ++i) {
+        if (--table[participant[i]] < 0) {
             answer = participant[i];
             break;
         }
     }
-    
     return answer;
 }
