b6a53ad8bb88e6be6ed8d8276e45ae540ad60bf9
GHW0826 <96938324+GHW0826@users.noreply.github.com>
2022-02-27 17:52:27 +0900

    Create 39.Combination_Sum.cpp

---
+++ LeetCode/medium/39.Combination_Sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> answer_;
+    vector<int> nums_;
+    int sum_;
+    int target_;
+    
+    void Sum(vector<int>& candidates, int start)
+    {
+        if (sum_ > target_)
+            return;
+        if (sum_ == target_) {
+            answer_.emplace_back(nums_);
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); ++i) {
+            if (sum_ < target_) {
+                sum_ += candidates[i];
+                nums_.push_back(candidates[i]);
+                Sum(candidates, i);
+                sum_ -= candidates[i];
+                nums_.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sum_ = 0;
+        target_ = target;
+        Sum(candidates, 0);
+        
+        return answer_;
+    }
+};
