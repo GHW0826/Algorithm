630bc900ffdef6a9d05f5d7972786632cdd65f90
GHW0826 <96938324+GHW0826@users.noreply.github.com>
2022-03-14 15:45:49 +0900

    Update 1149.cpp

--- bj/1149.cpp
+++ bj/1149.cpp
@@ -15,26 +15,20 @@ int main()
     cout.tie(nullptr);
 
     memset(dp, -1, sizeof(dp));
-
+    dp[0][0] = 0; dp[0][1] = 0; dp[0][2] = 0;
+    
     int caseNum = 0;
     cin >> caseNum;
-
-    for (int i = 0; i < caseNum; ++i) {
+    for (int i = 1; i <= caseNum; ++i) {
         int r = 0, g = 0, b = 0;
         cin >> r >> g >> b;
-        if (i > 0) {
-            dp[i][0] = r + min(dp[i - 1][1], dp[i - 1][2]);
-            dp[i][1] = g + min(dp[i - 1][0], dp[i - 1][2]);
-            dp[i][2] = b + min(dp[i - 1][0], dp[i - 1][1]);
-        }
-        else {
-            dp[i][0] = r;
-            dp[i][1] = g;
-            dp[i][2] = b;
-        }
+        
+        dp[i][0] = r + min(dp[i - 1][1], dp[i - 1][2]);
+        dp[i][1] = g + min(dp[i - 1][0], dp[i - 1][2]);
+        dp[i][2] = b + min(dp[i - 1][0], dp[i - 1][1]);
     }
 
-    int answer = min(dp[caseNum - 1][0], min(dp[caseNum - 1][1], dp[caseNum - 1][2]));
+    int answer = min(dp[caseNum][0], min(dp[caseNum][1], dp[caseNum][2]));
     cout << answer << endl;
 
     return 0;
