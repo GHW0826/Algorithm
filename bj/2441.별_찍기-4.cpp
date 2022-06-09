e0d392e75180cf27a549189565056bda35e2aeaf
GHW0826 <96938324+GHW0826@users.noreply.github.com>
2022-06-09 23:28:56 +0900

    Create 2441.별_찍기-4.cpp

---
+++ bj/2441.별_찍기-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int cnt;
+    cin >> cnt;
+    
+    for (int i = 0; i < cnt; ++i) {
+        for (int j = cnt - i; j < cnt; ++j)
+            cout << " ";
+        for (int j = i; j < cnt; ++j)
+            cout << "*";
+        cout << "\n";
+    }
+    
+    return 0;
+}
