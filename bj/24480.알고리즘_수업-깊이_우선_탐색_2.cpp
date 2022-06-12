926d2cb864b6c2bc6a6644bb9e798025e203f778
GHW0826 <96938324+GHW0826@users.noreply.github.com>
2022-06-12 20:06:31 +0900

    Create 24480.알고리즘_수업-깊이_우선_탐색_2.cpp

---
+++ bj/24480.알고리즘_수업-깊이_우선_탐색_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<int> order;
+vector<bool> visit;
+vector<set<int, std::greater<int>>> graph;
+int orders = 1;
+void dfs(vector<set<int, std::greater<int>>>& graph, vector<bool>& visit, int start)
+{
+    visit[start] = true;
+    order[start] = orders++;
+    for (auto& it : graph[start])
+        if (visit[it] == false)
+            dfs(graph, visit, it);
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int node, line, start;
+    cin >> node >> line >> start;
+
+    order.resize(node + 1, 0);
+    visit.resize(node + 1, false);
+    graph.resize(node + 1, set<int, std::greater<int>>());
+    int node1, node2;
+    for (int i = 0; i < line; ++i) {
+        cin >> node1 >> node2;
+        graph[node1].insert(node2);
+        graph[node2].insert(node1);
+    }
+
+    dfs(graph, visit, start);
+    for (int i = 1; i < order.size(); ++i)
+        cout << order[i] << "\n";
+
+    return 0;
+}
