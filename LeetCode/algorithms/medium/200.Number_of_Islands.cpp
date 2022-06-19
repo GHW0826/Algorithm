d86bc1477e88181d1b3efcede096de01fbf79ebe
KSS <96938324+GHW0826@users.noreply.github.com>
2022-06-19 19:07:42 +0900

    Update 200.Number_of_Islands.cpp

--- LeetCode/algorithms/medium/200.Number_of_Islands.cpp
+++ LeetCode/algorithms/medium/200.Number_of_Islands.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
+    int dx[4] = { -1,  0,  1,  0 };
+    int dy[4] = { 0, -1,  0,  1 };
+    vector<vector<bool>> visit;
+
     bool InvalidPos(vector<vector<char>>& grid, int x, int y)
-    { return !(x < 0 || x > grid[0].size() - 1 || y < 0 || y > grid.size() - 1); }
-
-    bool visitCheck(vector<vector<char>>& grid, vector<vector<bool>>& visit, int x, int y)
-    { return (InvalidPos(grid, x, y) == true) && grid[y][x] == '1' && visit[y][x] == false; }
-
-    int visit_lands(vector<vector<char>>& grid, vector<vector<bool>>& visit, int x, int y)
     {
-        int dx[4] = { -1, 0, 1, 0 };
-        int dy[4] = { 0, -1, 0, 1 };
+        return !(x < 0 || x > grid[0].size() - 1 || y < 0 || y > grid.size() - 1);
+    }
 
-        grid[y][x] = '0';
+    int visit_lands(vector<vector<char>>& grid, int x, int y)
+    {
         visit[y][x] = true;
         for (int dir = 0; dir < 4; ++dir) {
             int next_x = x + dx[dir];
             int next_y = y + dy[dir];
-            if (visitCheck(grid, visit, next_x, next_y) == true)
-                visit_lands(grid, visit, next_x, next_y);
+            if (InvalidPos(grid, next_x, next_y) == true)
+                if (visit[next_y][next_x] == false && grid[next_y][next_x] == '1')
+                    visit_lands(grid, next_x, next_y);
         }
         return 0;
     }
 
 
-    int numIslands(vector<vector<char>>& grid)
+    int numIslands(vector<vector<char>> grid)
     {
-        vector<vector<bool>> visit;
-        for (int i = 0; i < grid.size(); ++i)
-            visit.emplace_back(vector<bool>(grid[0].size(), false));
+        visit.resize(grid.size(), vector<bool>(grid[0].size(), false));
 
         int answer = 0;
         for (int i = 0; i < grid.size(); ++i) {
             for (int j = 0; j < grid[0].size(); ++j) {
-                if (visitCheck(grid, visit, j, i) == true) {
-                    visit_lands(grid, visit, j, i);
-                    ++answer;
+                if (InvalidPos(grid, j, i) == true) {
+                    if (visit[i][j] == false && grid[i][j] == '1') {
+                        visit_lands(grid, j, i);
+                        ++answer;
+                    }
                 }
             }
         }
