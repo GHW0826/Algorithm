d7d831b2201c95202328c8091e0426bb100aab28
KSS <96938324+GHW0826@users.noreply.github.com>
2022-11-02 00:36:12 +0900

    Create 3월에_태어난_여성_회원_목록_출력하기.sql

---
+++ programmers/level2/3월에_태어난_여성_회원_목록_출력하기.sql
@@ -0,0 +1,4 @@
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') DATE_OF_BIRTH
+from MEMBER_PROFILE
+where TLNO is not null and GENDER = 'W' and month(DATE_OF_BIRTH) = 3
+order by MEMBER_ID asc;
