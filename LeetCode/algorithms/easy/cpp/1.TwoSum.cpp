e916ae883a2101efa1f4c7089f6a01be2372eebb
KSS <96938324+GHW0826@users.noreply.github.com>
2023-04-15 21:07:01 +0900

    Create 1.TwoSum.cpp

---
+++ LeetCode/algorithms/easy/cpp/1.TwoSum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> table;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (table.find(target - nums[i]) != table.end())
+                return { table[target - nums[i]], i };
+            table[nums[i]] = i;
+        }
+        return {};
+    }
+};
